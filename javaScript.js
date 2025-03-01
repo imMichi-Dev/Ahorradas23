5a85fa2dfa7a85d8d0c8847e8ad2ba2eebc16e83
Michi Dev <59721533+Im-majo-jojo@users.noreply.github.com>
2025-03-01 02:49:22 -0500

    Utilities setup and repetitive functions stablishment.

--- javaScript.js
+++ javaScript.js
@@ -1,52 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
-    actualizarBalance();
-});
+// UTILITIES
+const $ =  (selector) => document.querySelector(selector);
 
-let operaciones = [];
+const $$ = (selector) => document.querySelectorAll(selector);
 
-function agregarOperacion() {
-    const descripcion = document.getElementById("descripcion").value;
-    const monto = parseFloat(document.getElementById("monto").value);
-    const tipo = document.getElementById("tipo").value;
+// ID DEFINITION FUNCTION
+const randomID = () => self.crypto.randomUUID()
 
-    if (!descripcion || isNaN(monto)) {
-        alert("Por favor, completa todos los campos.");
-        return;
+// LOCAL STORAGE
+const getData = (key) => JSON.parse(localStorage.getItem(key))
+
+const setData = (key, data) => localStorage.setItem(key, JSON.stringify(data))
+
+const clearTable = (selector) => $(selector).innerHTML = '';
+
+const defaultCategories = [
+    {
+        id: randomID(),
+        name: "comida",
+    },
+    {
+        id: randomID(),
+        name: "servicios"
+    },
+    {
+        id: randomID(),
+        name: "salidas"
+    },
+    {
+        id: randomID(),
+        name: "educacion"
+    },
+    {
+        id: randomID(),
+        name: "transporte"
+    },
+    {
+        id: randomID(),
+        name: "trabajo"
     }
-
-    const operacion = { descripcion, monto, tipo };
-    operaciones.push(operacion);
-    actualizarLista();
-    actualizarBalance();
-}
-
-function actualizarLista() {
-    const lista = document.getElementById("lista-operaciones");
-    lista.innerHTML = "";
-    operaciones.forEach((op, index) => {
-        const li = document.createElement("li");
-        li.classList = "flex justify-between bg-gray-100 p-2 rounded mt-2";
-        li.innerHTML = `
-            <span>${op.descripcion} - <strong class="${op.tipo === 'gasto' ? 'text-red-500' : 'text-green-500'}">
-                ${op.tipo === 'gasto' ? '-' : '+'}$${op.monto}
-            </strong></span>
-            <button onclick="eliminarOperacion(${index})" class="text-red-500">X</button>
-        `;
-        lista.appendChild(li);
-    });
-}
-
-function actualizarBalance() {
-    let total = 0;
-    operaciones.forEach(op => {
-        total += op.tipo === "gasto" ? -op.monto : op.monto;
-    });
-
-    document.getElementById("balance").textContent = `$${total}`;
-}
-
-function eliminarOperacion(index) {
-    operaciones.splice(index, 1);
-    actualizarLista();
-    actualizarBalance();
-}
+]
